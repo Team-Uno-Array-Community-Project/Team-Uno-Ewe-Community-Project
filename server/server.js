55955c389d560285b221e914fa571be1bd029a4f
Vladimir Weinstein <vlad@vladimirweinstein.com>
2021-05-11 14:34:00 -0400

    added require env in server js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const PORT = 3003;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
