791c777d01623af836adfdd49be7c7b9ad537f79
Eli Burgess <eburgess.nexstep@gmail.com>
2021-05-19 14:31:22 -0600

    added mongo atlas database

--- server/data/database.js
+++ server/data/database.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
 
-const server = `127.0.0.1:27017`;
-const database = `Ewe-count`;
+const server = 'mongodb+srv://Admin_Ewe:Array2021TeamEwe@team-ewe.rwk2g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// `127.0.0.1:27017`;
+// const database = `Ewe-count`;
 
 class Database {
     constructor() {
@@ -9,7 +10,7 @@ class Database {
     }
 
     _connect() {
-        mongoose.connect(`mongodb://${server}/${database}`,
+        mongoose.connect(`mongodb://${server}`,
             { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
             .then(() => {
                 console.log('Database connection successful.')
