adbae38474437079dfd996ae120f45930cd7ea8b
Eli Burgess <eburgess.nexstep@gmail.com>
2021-05-21 14:25:25 -0600

    cleaned up server and set semantic commenting for most of server folder, login, logout.jsx files in the src/admin folder

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
+//setting up callback points for necessary dependencies
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const PORT = 3003;
-
-require("dotenv").config();
 const mongoose = require('mongoose');
 
+// configuring the dotenv code to be used
+require("dotenv").config();
+
+//setting up the app to have access to called dependencies
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors());
 
+//setting the respective routes for fetch requests to the server
 app.use('/api/blog', require('./routes/blogs.route'));
 app.use('/api/item', require('./routes/items.route'));
 app.use('/api/admin', require('./routes/admin-route'));
