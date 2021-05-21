9d05e124dc23749c4c201b5a974327a7e7506419
Eli Burgess <eburgess.nexstep@gmail.com>
2021-05-21 10:13:42 -0600

    styled login page and set a conditional rendering logout button

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -3,11 +3,21 @@ import { useLocation, Link } from "react-router-dom";
 import { Navbar, Nav, Container, Image, Row, Col } from "react-bootstrap";
 
 import logo from "../assets/Ewe_Logo_Mint.png";
+import { render } from "@testing-library/react";
+import LogoutForm from "./admin/Logout";
 
 // useLocation is here to eventually display the current location of the user on the site. //
 
 const Navigation = (props) => {
   const location = useLocation();
+  let token = localStorage.getItem("token")
+
+  const logoutButton = () => {
+    if (token) {
+      return <LogoutForm />
+    }
+  }
+
   return (
     <>
       <Container className="company-logo-signin-container">
@@ -32,10 +42,12 @@ const Navigation = (props) => {
         <Nav className="nav-links">
           
           <Nav.Link href="/featured-products">Featured Products</Nav.Link>
-          
           <Nav.Link href="/blog">Blog</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
+        
+          {logoutButton()}
+          
         </Nav>
       </Navbar>
     </>
